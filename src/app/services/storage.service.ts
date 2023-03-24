aa17d7fd4f0ddf12413ac9db69f842a52ed2c536
Max Peintner <max@caos.ch>
2023-03-24 09:52:03 +0100

    use refresh token

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -4,39 +4,39 @@ import { OAuthStorage } from 'angular-oauth2-oidc';
 const STORAGE_PREFIX = 'zitadel';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class StorageService implements OAuthStorage {
-    private storage: Storage = window.sessionStorage;
+  private storage: Storage = window.localStorage;
 
-    constructor() { }
+  constructor() {}
 
-    public setItem<TValue = string>(key: string, value: TValue): void {
-        this.storage.setItem(this.getPrefixedKey(key), JSON.stringify(value));
+  public setItem<TValue = string>(key: string, value: TValue): void {
+    this.storage.setItem(this.getPrefixedKey(key), JSON.stringify(value));
+  }
+
+  public getItem<TResult = string>(key: string): TResult | null {
+    const result = this.storage.getItem(this.getPrefixedKey(key));
+    if (result) {
+      return JSON.parse(result);
     }
+    return null;
+  }
 
-    public getItem<TResult = string>(key: string): TResult | null {
-        const result = this.storage.getItem(this.getPrefixedKey(key));
-        if (result) {
-            return JSON.parse(result);
-        }
-        return null;
-    }
+  public removeItem(key: string): void {
+    this.storage.removeItem(this.getPrefixedKey(key));
+  }
 
-    public removeItem(key: string): void {
-        this.storage.removeItem(this.getPrefixedKey(key));
-    }
-
-    public getPrefixedKey(key: string): string {
-        return `${STORAGE_PREFIX}:${key}`;
-    }
+  public getPrefixedKey(key: string): string {
+    return `${STORAGE_PREFIX}:${key}`;
+  }
 }
 
 export class StorageConfig {
-    clientId: string = '';
-    storage: Storage = window.sessionStorage;
+  clientId: string = '';
+  storage: Storage = window.sessionStorage;
 }
 
 export enum StorageKey {
-    organization = 'organization',
+  organization = 'organization',
 }
