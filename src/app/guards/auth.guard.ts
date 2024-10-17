a536dd24f89b3fb4617d39349f61d4aa26b3615d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-10-16 22:10:00 -0500

    Angular 15 → 18

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthenticationService } from '../services/authentication.service';
@@ -8,7 +8,7 @@ import { AuthenticationService } from '../services/authentication.service';
 @Injectable({
     providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
     constructor(private auth: AuthenticationService) { }
 
     public canActivate(
